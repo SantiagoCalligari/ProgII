7ec2370c8fc7044f1304b1fd76df3bba8463f16f
Santiago Calligari <89709491+SantiagoCalligari@users.noreply.github.com>
2024-01-16 23:33:45 -0300

    empezamos el parse de todos los archivos dentro de autor

--- main.c
+++ main.c
@@ -4,16 +4,36 @@
 #define BUFF_SIZE 255
 #define WORK_DIR "./Textos/"
 
+int clean_and_unify_files( char *nombre_autor){
+  char file_list[BUFF_SIZE];
+  FILE *fp = fopen(file_list, "r");
+
+  snprintf(file_list, sizeof(file_list), "%s%s/%s", WORK_DIR, nombre_autor, ".list");
+  printf("Limpiando y unificando archivos de /%s\n", nombre_autor);
+  if (fp == NULL){
+    printf("Error al abrir el archivo %s\n", file_list);
+    return 1;
+  }
+  char file[BUFF_SIZE];
+
+  while(fgets(file, sizeof(file), fp) != NULL ) {
+    printf("%s", file);
+  }
+  return 1;
+}
 
 int create_file_list(char* nombre_autor){
   char command[BUFF_SIZE];
+  printf("Procesando %s\n", nombre_autor);
   snprintf(command, sizeof(command), "ls %s%s > %s%s/%s", WORK_DIR, nombre_autor, WORK_DIR, nombre_autor, ".list");
   int result = system(command);
-  if (result == -1) {
-      perror("Error al ejecutar el comando");
-  }
 
-  return result;
+  if ( result != 0){
+    printf("Ha ocurrido un error\n");
+    return result;
+  }
+  clean_and_unify_files(nombre_autor);
+  return 0;
 }
 
 
@@ -23,4 +43,5 @@ int main(int argc, char** argv){
     return 1;
   }
   create_file_list(argv[1]);
+  return 0;
 }
