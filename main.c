06c112d0e30bde929b046776b38f1f162249bf13
Santiago Calligari <89709491+SantiagoCalligari@users.noreply.github.com>
2024-01-17 19:05:09 -0300

    termine la creacion de los archivos de los autores

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <sys/stat.h>
 #include <ctype.h>
 #include <sys/types.h>
-#define BUFF_SIZE 511
+#define BUFF_SIZE 512
 #define WORK_DIR "./Textos"
 #define ENTRY_DIR "./Entradas"
 
@@ -12,55 +12,63 @@ void create_entry_folder(){
   struct stat st = {0};
   if (stat(ENTRY_DIR, &st) == -1) {
     mkdir(ENTRY_DIR, 0744);
+  }else{
+    remove(ENTRY_DIR);
+    mkdir(ENTRY_DIR, 0744);
   }
 }
 
-
 char *sanitize_string(char *str) {
-  int i = 0;
-  int j = 0;
-  while (str[i]) {
-    if (str[i] != '\n') {
-      str[i] = tolower(str[i]);
-      if ((str[i] >= 'a' && str[i] <= 'z') || str[i] == ' ' || str[i] == '.') {
-        str[j] = str[i];
-        j++;
-      }
+  int i = 0, j = 0;
+  for (i = 0; str[i] != '.'; i++) {
+    str[j] = tolower(str[i]);
+    if (( str[j] == '\n' ) && ((str[j-1] >= 97 && str[j-1] <= 122) || (str[j+1] >= 97 && str[j+1] <= 122))) {
+          str[j] = ' ';
     }
-    i++;
+    if( (str[j] >= 97 && str[j] <= 122) || str[j] == ' '){
+      j++;
+    }
+    
   }
-  str[j] = '\0';
+  str[j] = '\n';
+  str[j+1] = '\0';
+  printf("%s",str);
   return str;
 }
 
 
+int sanitize_file(char *nombre_autor, char *nombre_archivo) {
+  char line[BUFF_SIZE], file[BUFF_SIZE], result[BUFF_SIZE], entrada[BUFF_SIZE];
 
-void sanitize_file(char *nombre_autor, char *nombre_archivo) {
-  char line[BUFF_SIZE], file[BUFF_SIZE];
   snprintf(file, sizeof(file), "%s/%s/%s", WORK_DIR, nombre_autor, nombre_archivo);
+  snprintf(entrada, sizeof(entrada), "%s/%s%s", ENTRY_DIR, nombre_autor, ".txt");
   file[strlen(file) - 1] = '\0';
+  entrada[strlen(entrada)] = '\0';
+  result[0] = '\0';
 
-  FILE *fp = fopen(file, "r");
-  if (fp == NULL) {
-    perror("Error al abrir el archivo");
-    return;
+  FILE *escrito_autor = fopen(file, "r");
+  FILE *archivo_entrada = fopen(entrada, "a");
+  if (escrito_autor == NULL || archivo_entrada == NULL){
+    perror("Error");
+    return 1;
   }
 
-  char result[BUFF_SIZE];
-
-  while (fgets(line, sizeof(line), fp) != NULL) {
+  while (fgets(line, sizeof(line), escrito_autor) != NULL) {
     char *punto = strchr(line, '.');
 
     if (punto != NULL) {
       strncat(result, line, punto - line + 1);
-      printf("%s\n", sanitize_string(result));
+      char *sanitized_result = sanitize_string(result);
+      fprintf(archivo_entrada, "%s", sanitized_result);
       result[0] = '\0';
     } else {
       strcat(result, line);
     }
   }
 
-  fclose(fp);
+  fclose(archivo_entrada);
+  fclose(escrito_autor);
+  return 0;
 }
 
 int process_file_list( char *nombre_autor){
